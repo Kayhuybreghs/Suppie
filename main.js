3d9d3a1e1797e1d69407bf96f3fec3e41ce82015
kay huybreghs <kayhuybreghs@icloud.com>
2025-08-12 22:48:31 +0200

    Updated main.js

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-import style.class name {
-  constructor(params) {
-    
-  }
-}
+import './style.css';.
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
