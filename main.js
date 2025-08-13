27b788f842456b3710f5e1bc3d0ba3fc937b245c
kay huybreghs <kayhuybreghs@icloud.com>
2025-08-13 07:50:52 +0200

    Updated main.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 import './style.css';
 import { setupCounter } from './counter.js'
-import './chart-interactions.js';
-import './floating-bubbles.js';
-import './faq.js';
-
+import './lazy-load-modules.js';
 
