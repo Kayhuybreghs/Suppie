1d76c38597352a2e2c2c7884238193094c49e357
kay huybreghs <kayhuybreghs@icloud.com>
2025-08-13 10:40:18 +0200

    Updated main.js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-import './faq.js';
-import './chart-interactions.js';
-import './floating-bubbles.js';
+window.addEventListener('DOMContentLoaded', () => {
+  if (document.querySelector('.floating-bubbles')) {
+    import('./floating-bubbles.js');
+  }
+});
 
+const faqSection = document.querySelector('#sFaqSidebar');
+if (faqSection) {
+  const observer = new IntersectionObserver((entries) => {
+    if (entries.some(e => e.isIntersecting)) {
+      import('./faq.js');
+      observer.disconnect();
+    }
+  });
+  observer.observe(faqSection);
+}
 
+const chart = document.getElementById('visitors-chart');
+if (chart) {
+  import('./chart-interactions.js');
+}
