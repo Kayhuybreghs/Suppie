95196941f7fa88154205df25b9d11602215682ed
kay huybreghs <kayhuybreghs@icloud.com>
2025-08-13 08:01:26 +0200

    Updated main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import './style.css';
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-import './lazy-load-modules.js';
+import './faq.js';
+import './chart-interactions.js';
+import './floating-bubbles.js';
+
 
